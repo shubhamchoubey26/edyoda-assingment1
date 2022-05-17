c40cdd2d5fda3edd0f3ae3e7f3b838a79df90005
shubhamchoubey26 <104992221+shubhamchoubey26@users.noreply.github.com>
2022-05-17 13:06:57 +0530

    Add files via upload

---
+++ asngmt_Q3.py
@@ -0,0 +1,14 @@
+
+numbers = list(int(x) for x in input().split())
+
+
+count_odd = 0
+count_even = 0
+for x in numbers:
+    if not x % 2:
+        count_even+=1
+    else:
+        count_odd+=1
+print("number of even numbers:",count_even) 
+print("number of odd numbers:",count_odd)
+
